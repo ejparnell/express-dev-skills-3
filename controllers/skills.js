57fafb564ea3d51110a437774325f213278d9df0
lmikszta1 <lmikszta@gmail.com>
2023-05-03 17:06:12 -0400

    add new skill, delete skill, and edit skill functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,17 +2,50 @@ const Skill = require('../models/skill')
 
 function index(req, res){
     res.render('skills/index', {
-        skills: Skill.getAll()
+        skills: Skill.getAll(),
+        title: 'All Skills'
     })
 }
 
 function show(req, res){
     res.render('skills/show', {
+        skill: Skill.getOne(req.params.id),
+        title: 'Skill Details'
+    })
+}
+
+function newSkill(req, res){
+    res.render('skills/new', { title: 'New Skill' })
+}
+
+function create(req, res){
+    Skill.create(req.body)
+    res.redirect('/skills')
+}
+
+function deleteSkill(req, res){
+    Skill.deleteOne(req.params.id)
+    res.redirect('/skills')
+}
+
+function edit(req, res){
+    res.render('skills/edit', {
+        title: 'Edit Skill',
         skill: Skill.getOne(req.params.id)
     })
 }
 
+function update(req, res){
+    Skill.update(req.params.id, req.body)
+    res.redirect(`/skills`)
+}
+
 module.exports = {
     index,
-    show
+    show,
+    newSkill,
+    create,
+    deleteSkill,
+    edit,
+    update,
 }
--- models/skill.js
+++ models/skill.js
@@ -14,7 +14,29 @@ function getOne(id){
     return skills.find(skill => skill.id === id)
 }
 
+function create(skill){
+    skill.id = Date.now() % 1000000
+    skill.level = 'beginner'
+    skills.push(skill)
+}
+
+function deleteOne(id){
+    id = parseInt(id)
+
+    const idx = skills.findIndex(skill => skill.id === id)
+    skills.splice(idx, 1)
+}
+
+function update(id, updatedSkill){
+    id = parseInt(id)
+    const skill = skills.find(skill => skill.id === id)
+    Object.assign(skill, updatedSkill)
+}
+
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update,
 }
